569a88fba6cff552672697b55e2fdceb3619a072
James T McDuffie <204040590@mail.ad.ge.com>
2020-10-21 21:30:12 -0700

    Add project files.

---
+++ JoinMyGuild/JoinMyGuild/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using JoinMyGuild.ViewModels;
+
+namespace JoinMyGuild.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
